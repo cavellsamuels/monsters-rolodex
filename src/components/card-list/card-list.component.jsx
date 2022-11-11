33620de8ca588d05df3bbda7b4d66c974543c36b
Cavell Singleton <cavellsingleton@IT-LAP-006.ManningStainton.local>
2022-11-11 16:52:33 +0000

    Seperating Components Into Seperate Files

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,20 @@
 import { Component } from "react";
 
+import './card-list.styles.css'
+import Card from "../card/card.component";
+
 class CardList extends Component {
     render() {
         // console.log("render from CardList")
         const { monsters } = this.props
 
         return (
-            <div>
-                {monsters.map((monster) => (
-                    <h1 key={monster.id}> {monster.name} </h1>
-                ))}
+            <div className="card-list">
+                {monsters.map((monster) => {
+                    return (
+                        <Card monster={monster}/>
+                    )
+                })}
             </div>
         )
     }
